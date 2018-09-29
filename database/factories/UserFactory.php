94f0b940378b557983ed25f6484f40239480326b
toun18 <jungraiman56@gmail.com>
2018-09-30 03:55:20 +0700

    Add Fake user :100:

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,22 +2,14 @@
 
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'user_fname' => $faker->firstNameMale,
+        'user_lname' => $faker->lastName,
+        'user_email' => $faker->unique()->safeEmail,
+        'role_id' => 1,
+        'class_id' => 1,
+        'user_password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        // 'user_remember_token' => str_random(10),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Factory('App\User',10)->create();
     }
 }
